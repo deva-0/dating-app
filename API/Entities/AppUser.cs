174f31404c0d0c8fe0f3cb046c273ab1fc8ce60f
Adrian Zaręba <dev.adrianzareba@gmail.com>
2021-07-12 01:26:10 +0200

    Add walking skeleton

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    /// <summary>
+    /// Entity which represents default app user.
+    /// </summary>
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
