1a9c4e1f476c881a6a2eb1c8a9fd0a656c6bf519
Adrian Zaręba <dev.adrianzareba@gmail.com>
2021-07-17 23:44:20 +0200

    Extend AppUser entity and implement Seed class to insert random testing data into the database

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,9 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    /// <summary>
-    /// api/users allows read access to SQLite database Users
-    /// </summary>
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -21,14 +19,11 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
-        // api/users/3
-        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
