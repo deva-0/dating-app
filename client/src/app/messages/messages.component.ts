4d90858e01ea7321ab520c74f53fffd612a73474
Adrian Zaręba <dev.adrianzareba@gmail.com>
2021-07-27 09:23:16 +0200

    Add user inbox skeleton

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import { MessageService } from '../_services/message.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
-  constructor() {}
+  messages: Message[];
+  pagination: Pagination;
+  container = 'Unread';
+  pageNumber = 1;
+  pageSize = 5;
 
-  ngOnInit(): void {}
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService
+      .getMessages(this.pageNumber, this.pageSize, this.container)
+      .subscribe((response) => {
+        this.messages = response.result;
+        this.pagination = response.pagination;
+      });
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event;
+    this.loadMessages();
+  }
 }
