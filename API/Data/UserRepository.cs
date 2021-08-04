6d7463b872dfa88c235dd256d5c21c90410da0d8
Adrian Zaręba <dev.adrianzareba@gmail.com>
2021-08-04 23:07:01 +0200

    Optimize database queries

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -97,6 +97,13 @@ namespace API.Data
                 .SingleOrDefaultAsync(x => x.UserName == username);
         }
 
+        public async Task<string> GetUserGender(string username)
+        {
+            return await _context.Users
+                .Where(x => x.UserName == username)
+                .Select(x => x.Gender).FirstOrDefaultAsync();
+        }
+
         /// <summary>
         ///     Retrieves all users with from database.
         /// </summary>
